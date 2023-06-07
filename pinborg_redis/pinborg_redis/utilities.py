ff4ad029153ee89e652bb4da08b7f1e7df77526c
Viton Vitanis <960755+notiv@users.noreply.github.com>
2023-06-07 23:44:37 +0200

    Replace beautifulsoup with trafilatura for parsing html

--- pinborg_redis/pinborg_redis/utilities.py
+++ pinborg_redis/pinborg_redis/utilities.py
@@ -1,8 +1,8 @@
 
 import io
 
-from bs4 import BeautifulSoup
 from pypdf import PdfReader
+from trafilatura import extract
 
 def parse_pdf(response):
     """ Parses a pdf url (response) and returns the content as text. Images and 
@@ -44,13 +44,6 @@ def parse_html(response):
         A string with the content of the html page (only text)
     """
 
-    soup = BeautifulSoup(response.body, 'html.parser')
-    # http://stackoverflow.com/questions/22799990/beatifulsoup4-get-text-still-has-javascript
-    for script in soup(["script", "style"]):
-        script.extract()
-    text = soup.get_text()
-    lines = (line.strip() for line in text.splitlines())
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    text = '\n'.join(chunk for chunk in chunks if chunk)
+    text = extract(response.body)
 
     return text
